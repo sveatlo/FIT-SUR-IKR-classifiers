d19c390cdc6aa9b29ea35bbd029752dbeeb3ef0c
Svätopluk Hanzel <svatoplukhanzel@pm.me>
2021-05-14 16:57:04 +0200

    feat(image): added cmv, knn, mlp classifiers

--- faces.py
+++ faces.py
@@ -6,6 +6,12 @@ from pathlib import Path
 import PIL
 import face_recognition
 import numpy as np
+from skimage.feature import hog
+from skimage.io import imread
+from sklearn.metrics import accuracy_score
+from sklearn.neural_network import MLPClassifier
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.svm import SVC
 
 class Dataloader():
     """ Dataloader is responsible for loading samples in a format for Recognizer """
@@ -71,6 +77,7 @@ class Recognizer():
         self.known_encodings.append(encodings[0])
         self.known_ids.append(face_id)
 
+
     def recognize(self, face_img_path):
         img = face_recognition.load_image_file(face_img_path)
         face_locations = face_recognition.face_locations(img)
@@ -85,42 +92,118 @@ class Recognizer():
 
         face_distances = face_recognition.face_distance(self.known_encodings, face_encoding)
         best_match_index = np.argmin(face_distances)
+
+        class_distances = {}
+        for i, dist in enumerate(face_distances):
+            face_id = self.known_ids[i]
+
+            if face_id not in class_distances:
+                class_distances[face_id] = dist
+
+            class_distances[face_id] = min(class_distances[face_id], dist)
+        distances = list(class_distances.values())
+
+        face_id = -1
         if matches[best_match_index]:
             face_id = self.known_ids[best_match_index]
-            return face_id, face_distances
-        else:
-            return -1, []
+
+        return face_id, distances
 
 
-def main():
-    recognizer = Recognizer()
+EVAL = True
 
+def face_recognition_main():
     dataset = Dataloader("./dataset")
 
+    recognizer = Recognizer()
     for (face_id, face_img_path) in dataset.train_samples():
         recognizer.learn(face_id, face_img_path)
 
-    print("======================================================")
+    print("======================================================", file=sys.stderr)
 
-    #  total, correct = 0, 0
-    #  for (face_id, face_img_path) in dataset.test_samples():
-    #      recognized_face_id, _ = recognizer.recognize(face_img_path)
-    #
-    #      total+=1
-    #      if face_id == recognized_face_id:
-    #          correct += 1
-    #
-    #  print(f"{correct}/{total}")
+    if not EVAL:
+        print("Running test", file=sys.stderr)
+        total, correct = 0, 0
+        for (face_id, face_img_path) in dataset.test_samples():
+            recognized_face_id, _ = recognizer.recognize(face_img_path)
 
-    for face_img_path in dataset.eval_samples():
-        recognized_face_id, distances = recognizer.recognize(face_img_path)
+            total+=1
+            if face_id == recognized_face_id:
+                correct += 1
 
-        sample_id = face_img_path.stem
+        print(f"{correct/total} ({correct}/{total})")
+    else:
+        with open('image_face_recognition.txt', 'w') as out:
+            for face_img_path in dataset.eval_samples():
+                recognized_face_id, distances = recognizer.recognize(face_img_path)
 
-        print(f"{sample_id} {recognized_face_id} {' '.join([str(d) for d in distances])}")
+                sample_id = face_img_path.stem
 
+                probs = ["NaN" for d in range(31)]
 
+                print(f"{sample_id} {recognized_face_id} {' '.join([str(d) for d in probs])}", file=out)
 
+def face_recognition_scikit(classifier='mlp'):
+    dataset = Dataloader("./dataset")
+
+    classifiers = {
+        'mlp': MLPClassifier(hidden_layer_sizes=(800,), activation='tanh', learning_rate='adaptive', alpha=1e-4, max_iter=400, random_state=1),
+        'svc': SVC(C=1e2, probability=True),
+        'knn': KNeighborsClassifier(),
+    }
+    clf = classifiers[classifier]
+
+    X_train = []
+    Y_train = []
+    for (face_id, face_img_path) in dataset.train_samples():
+        img = imread(face_img_path, plugin='pil')
+        out, hog_img = hog(img, orientations=8, pixels_per_cell=(8, 8), cells_per_block=(1, 1), block_norm='L1', visualize=True)
+        Y_train.append(face_id)
+        X_train.append(out)
+
+    clf = clf.fit(X_train, Y_train)
+
+    print("======================================================", file=sys.stderr)
+
+    if not EVAL:
+        print(f"Running test [{classifier}]", file=sys.stderr)
+        X_test = []
+        Y_test = []
+        for (face_id, face_img_path) in dataset.test_samples():
+            img = imread(face_img_path, plugin='pil')
+            out, hog_img = hog(img, orientations=8, pixels_per_cell=(8, 8), cells_per_block=(1, 1), block_norm='L1', visualize=True)
+            Y_test.append(face_id)
+            X_test.append(out)
+
+        Y_pred = clf.predict(X_test)
+
+        total, correct = 0, 0
+        for gt, pred in zip(Y_test, Y_pred):
+            total += 1
+            if gt == pred:
+                correct += 1
+        correct, total, correct/total
+
+        print(f"{correct/total} ({correct}/{total})")
+    else:
+        names = []
+        X_eval = []
+        for face_img_path in dataset.eval_samples():
+            img = imread(face_img_path, plugin='pil')
+            out, hog_img = hog(img, orientations=8, pixels_per_cell=(8, 8), cells_per_block=(1, 1), block_norm='L1', visualize=True)
+            names.append(face_img_path.stem)
+            X_eval.append(out)
+
+        pred_proba = clf.predict_proba(X_eval)
+
+        with open('image_{}.txt'.format(classifier), 'w') as out:
+            for name, proba in zip(names, pred_proba):
+                print(name,
+                      1 + np.argmax(proba),
+                      ' '.join([str(x) for x in proba.tolist()]),
+                      file=out)
 
 if __name__ == "__main__":
-    main()
+    face_recognition_main()
+    for classifier in ('mlp', 'svc', 'knn'):
+        face_recognition_scikit(classifier)
